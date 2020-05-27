54e60b8dc67470bf4d6e605c1dbc03fb3b6a5f5c
marina-andrijevic <55096107+marina-andrijevic@users.noreply.github.com>
2020-05-27 12:16:30 +0200

    upload

---
+++ Code/gii_data_analysis.R
@@ -0,0 +1,348 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # #  Gender Equality Extension of SSPs  # # # # # # # # # # 
+# # # # # # #           Data Processing           # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+rm(list = ls())
+
+library(ggplot2)
+library(tidyverse)
+library(broom)
+library(countrycode)
+library(readstata13)
+library(sandwich)
+library(lmtest)
+library(zoo)
+library(plm)
+library(broom)
+library(readxl)
+library(wrapr)
+library(ggpmisc)
+library(ggpubr)
+library(gridExtra)
+
+setwd('/Users/marinaandrijevic/PhD/Gender equality projections/Data/Input')
+
+
+# 1. Gender Inequality Index (GII) - replication based on the UNDP methodology
+
+custom.match <- c(`Eswatini (Kingdom of)` = 'SWZ') # Eswatini/Swaziland custom code for the countrycode package
+
+# Load the UNDP data
+
+gii.hist <- read.csv('gii_historical.csv', skip = 1) %>% 
+  select(Country, X1995:X2018) %>% 
+  mutate(countrycode = countrycode(Country, 'country.name', 'iso3c', custom_match = custom.match)) %>%
+  gather(year, gii.hist, -countrycode, -Country) %>%
+  mutate(year = year %>% str_replace("X","") %>% as.integer(),
+         gii.hist = gii.hist %>% as.numeric()) %>% 
+  filter(!year %in% c('.', 0)) %>% 
+  select(-Country)
+
+# Components: 
+
+# Health (female reproductive health index): maternal mortality ratio, adolescent birth rate
+
+custom.match <- c(`Eswatini (Kingdom of)` = 'SWZ') # Eswatini/Swaziland custom code for the countrycode package
+
+mmr.undp <- read.csv('mmr.csv', skip = 1) %>% 
+  select(Country, X1995:X2015) %>%
+  mutate(countrycode = countrycode(Country, 'country.name', 'iso3c', custom_match = custom.match)) %>%
+  gather(year, mmr, -countrycode, -Country) %>%
+  mutate(year = year %>% str_replace("X","") %>% as.integer(),
+         mmr = mmr %>% as.numeric()) %>% 
+  filter(!year %in% c('.', 0) & year < 2000) %>% 
+  drop_na(countrycode) %>% 
+  select(-Country) %>% 
+  filter(year < 2000)
+
+mmr.wb <- read.csv('mmr_wb.csv', skip = 3) %>% 
+  select(Country.Code, X2000:X2018) %>%
+  rename(countrycode = Country.Code) %>% 
+  gather(year, mmr, -countrycode) %>% 
+  mutate(year = year %>% str_replace("X","") %>% as.integer())
+
+mmr <- mmr.undp %>% 
+  bind_rows(mmr.wb) 
+
+abr <- read.csv('abr_wb.csv', skip = 3) %>% 
+  select(Country.Code, X1990:X2018) %>%
+  rename(countrycode = Country.Code) %>% 
+  gather(year, abr, -countrycode) %>% 
+  mutate(year = year %>% str_replace("X","") %>% as.integer())
+
+
+# Empowerment: female and male population with at least secondary education, female and male share of parliamentary seats = female/male empowerment index
+
+singleentry <- c('ATG', 'BRB', 'BRN', 'ERI', 'ESH', 'GRD', 'GUM', 'LBY', 'MRT', 'MYT', 'PNG', 'UZB', 'VIR', 'DJI', 'SYC') #will be deleted from the sample as the values are only for 2015 and this prevents interpolation
+
+edu.prep <- read.csv('edu_mf.csv', skip = 8) %>% 
+  rename_all(tolower) %>% 
+  mutate(countrycode = countrycode(area, 'country.name', 'iso3c'),
+         education = str_replace(str_to_lower(education), fixed(" "), ".")) %>% 
+  filter(education %in% c('lower.secondary', 'upper.secondary', 'post.secondary')) %>% 
+  drop_na(countrycode) %>% 
+  group_by(scenario, countrycode, year, sex) %>% 
+  mutate(agg.edu = sum(distribution, na.rm = T)/100) %>% 
+  ungroup() %>% 
+  filter(education == 'upper.secondary',
+        !countrycode %in% singleentry)
+
+dpinterp <- function(df) {
+  with(df, approx(x=indx, y=value, xout=min(indx):max(indx))) %>% 
+    bind_cols()
+}
+
+edu <- edu.prep %>% 
+  select(countrycode, year, scenario, sex, agg.edu) %>% 
+  group_by(scenario, countrycode, sex) %>% 
+  rename(indx = year, value = agg.edu) %>% 
+  filter(!indx == 2015) %>% 
+  do(dpinterp(.)) %>% 
+  rename(year = x, agg.edu = y) %>% 
+  right_join(edu.prep %>% distinct(countrycode), by='countrycode') %>% 
+  spread(sex, agg.edu) %>% 
+  rename(edu.f = Female, edu.m = Male) %>% 
+  filter(year < 2020 & scenario == 'SSP2')
+
+
+parl1 <- read.csv('fem_parliament_seats.csv', skip = 1) %>%
+  select(Country, X1995:X2018) %>%
+  mutate(countrycode = countrycode(Country, 'country.name', 'iso3c', custom_match = custom.match)) %>%
+  gather(year, parl.f, -countrycode, -Country) %>%
+  mutate(year = year %>% str_replace("X","") %>% as.integer(),
+         parl.f = (parl.f %>% as.numeric())/100) %>%
+  filter(!year %in% c('.', 0)) %>%
+  select(-Country) %>% 
+  mutate(id = paste0(countrycode, year))
+
+
+parl2 <- read.csv('seats-held-by-women-in-national-parliaments.csv', na.strings = "") %>% 
+  rename(parl.f = 'Proportion.of.seats.held.by.women.in.national.parliaments........',
+         countrycode = Code,
+         year = Year) %>% 
+  mutate(parl.f = parl.f/100) %>% 
+  drop_na(countrycode) %>% 
+  mutate(id = paste0(countrycode, year))
+
+
+parl <- merge(parl1, parl2, by = "id", all = TRUE) %>% 
+  mutate(parl.f = (parl.f.x %?% parl.f.y),  #fill the missing values from one dataset into the other
+         year = year.x %?% year.y) %>% 
+  select(id, parl.f) %>% 
+  mutate(countrycode = substr(id,1,3),
+         year = substr(id,4,8) %>% as.integer())
+
+
+# Labour market: female and male LFPR; female/male labour market index
+
+flfp <- read.csv('flfp_ilo_mod_est.csv', skip = 3) %>% 
+  select(Country.Code, X1990:X2018) %>%
+  rename(countrycode = Country.Code) %>% 
+  gather(year, flfp, -countrycode) %>% 
+  mutate(year = year %>% str_replace("X","") %>% as.integer(),
+         flfp = flfp/100)
+
+mlfp <- read.csv('mlfp_ilo_mod_est.csv', skip = 3) %>% 
+  select(Country.Code, X1990:X2018) %>%
+  rename(countrycode = Country.Code) %>% 
+  gather(year, mlfp, -countrycode) %>% 
+  mutate(year = year %>% str_replace("X","") %>% as.integer(),
+         mlfp = mlfp/100)
+
+
+# Put together to calculate the index 
+
+gii.prep <- mmr %>% 
+  left_join(abr, by = c('countrycode', 'year')) %>% 
+  left_join(edu, by = c('countrycode', 'year')) %>% 
+  left_join(parl, by = c('countrycode', 'year')) %>% 
+  left_join(flfp, by = c('countrycode', 'year')) %>% 
+  left_join(mlfp, by = c('countrycode', 'year')) %>% 
+  mutate(mmr = ifelse(mmr >= 1000, 1000, mmr), 
+         mmr = ifelse(mmr <= 10, 10, mmr),
+         parl.f = ifelse(parl.f == 0, 0.01, parl.f),
+         parl.m = 1 - parl.f)
+
+# Add agegrated health, empowerment and LFPR dimensions
+
+gii.master <- gii.prep %>% 
+  mutate(health = ((10/mmr  * 1/abr)^1/2 + 1)/2,
+         empower = ((parl.f*edu.f)^(1/2) + (parl.m*edu.m)^(1/2))/2,
+         lfpr = (flfp + mlfp)/2,
+         gf = ((10/mmr * 1/abr)^(1/2) * (parl.f*edu.f)^(1/2) * flfp)^(1/3),
+         gm = ((parl.m*edu.m)^(1/2) * mlfp)^(1/3),
+         gfm = (health * empower * lfpr)^(1/3),
+         harm = 1/((1/gf + 1/gm)/2),
+         gii = 1 - harm/gfm, 
+         gii = ifelse(gii < 0, 0.01, gii)) %>% 
+  drop_na(gii)
+
+# Even though the UNDP gives data for 2018 as well, they use the maternal mortality ratio from 2015,
+# and our analysis stops at 2017 because that's the latest for the MMR data
+
+# Graphs for validation of the replicated index vs the original
+
+validation <- gii.master %>% 
+  left_join(gii.hist, by = c('countrycode', 'year')) %>% 
+  arrange(countrycode, desc(countrycode))
+
+s1 <- c(unique(validation$countrycode)[1:30])
+s2 <- c(unique(validation$countrycode)[31:60])
+s3 <- c(unique(validation$countrycode)[61:90])
+s4 <- c(unique(validation$countrycode)[91:120])
+s5 <- c(unique(validation$countrycode)[121:150])
+s6 <- c(unique(validation$countrycode)[151:170])
+
+ggplot(validation %>% filter(countrycode %in% s6)) + 
+  geom_line(aes(year, gii.hist, color = 'UNDP')) +
+  geom_line(aes(year, gii, color = 'Replication')) + 
+  labs(title = 'Gender Inequality Index replication', color = 'Index version',  y = "GII", x = "Year") +
+  facet_wrap(~countrycode) +
+  theme(axis.text.x = element_text(angle = 90), legend.position = 'bottom') +
+  ylim(0, 1) + 
+  scale_color_manual(values=c("#4D9221", "#8E0152"))
+
+
+#write.csv(gii.master, '/Users/marinaandrijevic/PhD/Gender equality projections/Data/Output/gii_replication.csv')
+
+
+# Comparison with the CLIMI index and vulnerability index (Fig 1):
+
+climi <- read.csv('climi.csv')
+
+vuln <- read.csv('ndgain_vulnerability.csv') %>% 
+  rename_all(tolower) %>% 
+  gather(year, vulnerability, -name, -iso3) %>% 
+  mutate(year = year %>% str_replace("x", "") %>% as.numeric) %>% 
+  rename(countrycode = iso3)
+
+comp <- gii.complete %>%
+  filter(year %in% 2007:2015) %>% # Gender inequality (2007 - 2015 average), to match the Climate Action Index
+  group_by(countrycode) %>% 
+  mutate(gii.avg = mean(gii)) %>% 
+  ungroup() %>% 
+  filter(year == 2010) %>% 
+  select(-year) %>% 
+  left_join(climi, by = 'countrycode') 
+
+comp2 <- gii.complete %>%
+  left_join(vuln, by = c('countrycode', 'year')) 
+
+
+f1 <- y ~ x
+
+ggplot(comp, aes(x = gii.avg, y = climi) ) +
+  geom_point() +
+  geom_smooth(method = 'lm', level = 0.95, formula = f1) +
+  stat_poly_eq(formula = f1,
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~~~")),
+               parse = TRUE) +
+  labs(y = "Climate action", x = "Gender inequality") + 
+  theme_minimal() +
+  theme(text = element_text(size=16),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_blank()) +
+  xlim(0, 0.7)
+
+
+ggplot(comp2 %>% filter(year == 2017), aes(x = gii, y = vulnerability)) +  # Latest availbale years for the two indices: 2017
+  geom_point() +
+  geom_smooth(method = 'lm', level = 0.95, color = 'orange', formula = f1) +
+  stat_poly_eq(formula = f1,
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~~~")),
+               parse = TRUE) +
+  labs(y = "Vulnerability", x = "Gender inequality") + 
+  theme_minimal() +
+  theme(text = element_text(size=16),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_blank()) +
+  xlim(0, 0.7)
+
+cor(comp$climi, comp$gii.avg, use = 'complete.obs')
+
+ggplot(comp) +
+  geom_point(aes(climi, gii.avg)) +
+  geom_smooth(aes(climi, gii.avg), method = lm, formula = y ~ splines::bs(x, 3)) +
+  labs(y = "Gender Inequality Index (2007-2015 average)", x = "Index of Climate Action", title = 'Less gender inequality -> more climate action?' )
+
+
+# Sensitivity analysis with other gender (in)equality indicators (Supplementary Material)
+
+comp <- gii.hist %>% filter(year == 2016 | year == 2017) %>% select(countrycode, year, gii.hist)
+
+# GDI Index
+
+gdi <- read.csv('gender_development_index_obs.csv') %>% 
+  left_join(gii.hist, by = c('countrycode', 'year')) %>% 
+  #left_join(soceco, by = c('countrycode', 'year')) %>% 
+  #mutate(mys.gap = ifelse(mys.gap < 1, 0, mys.gap)) %>% 
+  #mutate(scenario = 'Observed') %>% 
+  drop_na(gdi) %>% 
+  mutate(gdi.log = log(gdi/(1-gdi))) %>% 
+  filter(year == 2017 & gdi > 0.7) # Excludes two biggest ouliers
+
+
+p1 <- ggscatter(gdi, x = 'gdi', y = 'gii.hist',
+          add = "reg.line", 
+          add.params = list(color = "blue", fill = "lightgray"),
+          conf.int = TRUE,
+          title = 'GDI vs GII (2017)', xlab = 'Gender Development Index', ylab = 'Gender Inequality Index') +
+  stat_cor(method = "pearson", label.y = 0.9, label.x = 0.8) +
+  scale_y_continuous(limits = c(0,1))
+
+# WPS Index
+
+wps16 <- read_excel('wps_2016_19.xlsx', sheet = '2016') %>% 
+  mutate(year = 2016)
+
+wps <- read_excel('wps_2016_19.xlsx', sheet = '2019') %>% 
+  mutate(year = 2017) %>% 
+  bind_rows(wps16) %>% 
+  mutate(countrycode = countrycode(country, 'country.name', 'iso3c', custom_match = custom.match)) %>% 
+  left_join(comp, by = c('countrycode', 'year')) %>% 
+  #left_join(soceco, by = c('countrycode', 'year')) %>% 
+  #mutate(mys.gap = ifelse(mys.gap < 1, 0, mys.gap)) %>% 
+  #mutate(scenario = 'Observed') %>% 
+  drop_na(wps) %>% 
+  mutate(wps.log = log(wps/(1-wps))) %>% 
+  filter(year == 2017)
+
+p2 <- ggscatter(wps, x = 'wps', y = 'gii.hist',
+          add = "reg.line", 
+          add.params = list(color = "blue", fill = "lightgray"),
+          conf.int = TRUE,
+          title = 'WPS Index vs GII (2017)', xlab = 'Women, Peace and Security Index', ylab = 'Gender Inequality Index') +
+  stat_cor(method = "pearson", label.y = 0.9, label.x = 0.6) +
+  scale_y_continuous(limits = c(0,1))
+
+
+# Global Gender Gap Index
+
+gg.wef <- read_excel('gggi_wef.xlsx') %>%
+  as.data.frame() %>% 
+  gather(year, gg.wef, -Country, -Region) %>% 
+  mutate(gg.wef = gg.wef %>% as.numeric(),
+         year = year %>% as.integer(),
+         countrycode = countrycode(Country, 'country.name', 'iso3c')) %>% 
+  left_join(gii.hist, by = c('countrycode', 'year')) %>% 
+  #left_join(soceco, by = c('countrycode', 'year')) %>% 
+  #mutate(mys.gap = ifelse(mys.gap < 1, 0, mys.gap)) %>% 
+  #mutate(scenario = 'Observed') %>% 
+  drop_na(gg.wef) %>% 
+  mutate(gg.wef.log = log(gg.wef/(1-gg.wef))) %>% 
+  filter(year == 2017)
+
+p3 <- ggscatter(gg.wef, x = 'gg.wef', y = 'gii.hist',
+          add = "reg.line", 
+          add.params = list(color = "blue", fill = "lightgray"),
+          conf.int = TRUE,
+          title = 'WEF Gender Gap vs GII (2017)', 
+          xlab = 'Gender Gap (World Economic Forum)', 
+          ylab = 'Gender Inequality Index') +
+  stat_cor(method = "pearson", label.y = 0.9, label.x = 0.6) +
+  scale_y_continuous(limits = c(0,1))
+
+grid.arrange(p1, p2, p3, nrow = 1, top = 'Comparison with other indices of gender equality')
+
+
---
+++ Code/gii_projections.R
@@ -0,0 +1,333 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # #  Gender Equality Extension of SSPs  # # # # # # # # # # 
+# # # # # # #              Projections            # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+rm(list = ls())
+
+library(ggplot2)
+library(tidyverse)
+library(broom)
+library(countrycode)
+library(readstata13)
+library(sandwich)
+library(lmtest)
+library(zoo)
+library(plm)
+library(broom)
+library(readxl)
+library(wrapr)
+library(stargazer)
+library(RColorBrewer)
+library(colorspace)
+
+setwd('/Users/marinaandrijevic/PhD/Gender equality projections/Data')
+
+# Load replicated GII
+
+gii.master <- read.csv('Output/gii_replication.csv')
+
+# Current range of the OECD countries (for cut-off values on figure legends)
+
+gii.current <- gii.master %>% filter(year == 2017)
+
+oecd <- gii.current %>% 
+  filter(countrycode %in% c("AUS", "AUT", "BEL", "CAN", "CHL", "CZE","DNK","EST","FIN","FRA","DEU","GRC",
+                            "HUN", "ISL", "IRL", "ITA", "JPN", "KOR", "LVA", "LUX", "MEX", "NLD", "NZL", 
+                            "NOR", "POL", "PRT", "SVK", "SVN", "ESP", "SWE", "CHE", "TUR", "GBR","USA")) 
+summary(oecd)
+
+# The range for OECD countries is GII 0.002 - 0.315; mean for all countries in the sample is 0.33, median 0.38
+
+# # # Regression analysis
+
+# Add covariates to the dataset: GDP, higher education, gap in mean years of schooling
+
+gdp <- read.dta13('Input/pwt91.dta') %>% 
+  select(countrycode:pop) %>% 
+  mutate(lngdppc = log(rgdpe/pop), 
+         lngdppcsq = lngdppc^2)
+
+soceco <- read.csv('Input/educ_vars_ipol.csv') %>%  # Education and governance data (previously assembled)
+  select(countrycode, year, post.secondary, mys.gap) %>% 
+  left_join(gdp, by = c('countrycode', 'year')) %>% 
+  filter(year %in% (1995:2018))
+
+gii.reg <- gii.master %>% 
+  left_join(soceco, by = c('countrycode', 'year')) %>% 
+  mutate(mys.gap = ifelse(mys.gap < 1, 0, mys.gap)) %>% 
+  mutate(scenario = 'Observed') %>% 
+  select(countrycode, year, gii, lngdppc, post.secondary, mys.gap) %>% 
+  na.omit() %>% 
+  mutate(gii.log = log(gii/(1-gii)))
+
+# Main regression specification (see Supplementary Material for more)
+
+reg.mod <- lm(gii.log ~ lngdppc + mys.gap + post.secondary + factor(countrycode), data = gii.reg)
+summary(reg.mod)
+
+
+# Projections with logistic transformation 
+
+# Out of sample data: 
+
+newcntry <- c('BRB', 'BRN', 'CUB', 'DJI', 'ERI', 'GRD', 'GUY', 'HKG', 'LBY', 'MRT', 'PNG', 'PRI', 'SLB', 'SOM', 'TLS', 'TON', 'UZB', 'VUT', 'WSM')
+
+df.prep <- data.frame(year = rep(2015,5), scenario = c('SSP1', 'SSP2', 'SSP3', 'SSP4', 'SSP5'))
+
+proj.prep <- read.csv('Input/gdp_edu_projections.csv') %>% 
+  select(countrycode, year, scenario, gdppc, postsec, mys.gap) %>%
+  filter(year == 2015 & scenario == 'SSP2') %>% 
+  select(-year, -scenario) %>% 
+  merge(df.prep)
+
+proj.dat <- read.csv('Input/gdp_edu_projections.csv') %>% 
+  filter(!year == 2015) %>% 
+  select(countrycode, year, scenario, gdppc, postsec, mys.gap) %>% 
+  bind_rows(proj.prep) %>% 
+  filter(!countrycode %in% newcntry) %>% 
+  mutate(mys.gap = ifelse(mys.gap < 1, 0, mys.gap),
+       lngdppc = log(gdppc)) %>% 
+  rename(post.secondary = postsec) 
+
+gii.proj.log <- proj.dat %>% 
+  mutate(gii.log.proj = predict(reg.mod, newdata = proj.dat), # Use predict function to derive out-of-sample projections
+         gii.proj = exp(gii.log.proj) / (1 + exp(gii.log.proj))) %>% # Transform back from log scale to 0-1
+  drop_na(countrycode) 
+
+gii.complete <- gii.master %>% 
+  filter(year <= 2017) %>% 
+  select(countrycode, year, gii) %>% 
+  mutate(scenario = 'Observed') %>% 
+  drop_na(gii) %>% 
+  bind_rows(gii.proj.log %>% filter(year >= 2015) %>% select(countrycode, scenario, year, gii.proj) %>% rename(gii = gii.proj))
+
+#write.csv(gii.complete, 'Output/gii_obs_proj.csv')
+
+sample <- sample(gii.complete$countrycode, size = 12)
+
+sample1 <- c('NOR', 'SWE', 'DEU', 'GBR', 'ITA')
+
+ggplot() + 
+  geom_line(data = gii.complete %>% filter(countrycode %in% sample1 & year <= 2018), aes(year, gii, color = 'Observed'), size = 1) +
+  geom_line(data = gii.complete %>% filter(countrycode %in% sample1 & year >= 2015), aes(year, gii, color = scenario)) +
+  facet_wrap(~countrycode) + 
+  labs(title = 'GII with log transformation') +
+  ylim(0, 1)
+
+
+# Average by region
+
+regions.developing <- read.csv('Input/ccodes_regions.csv') %>% 
+  mutate(region = recode(region, 'LDC' = 'LDCs'))
+
+regions.developed <- read.csv('Input/regions.csv') %>% 
+  filter(region %in% c('North America', 'Europe', 'East Asia & Pacific')) %>% 
+  mutate(region = recode(region, 'North America' = "Europe & North America", "Europe" = "Europe & North America"))
+
+regions <- regions.developing %>% 
+  bind_rows(regions.developed)
+
+gii.region <- gii.complete %>% 
+  left_join(regions, by = 'countrycode') %>% 
+  group_by(region, year, scenario) %>%
+  mutate(gii.mean = mean(gii)) %>% 
+  ungroup() %>% 
+  drop_na(region)
+
+
+# Main regional plot (Fig 3)
+
+gii.region$scenario <- factor(gii.region$scenario, c("Observed", "SSP5", "SSP2", "SSP3", "SSP4", "SSP1"))
+
+ggplot() +
+  geom_point(data = gii.region %>%  filter(year <= 2015 & scenario == 'Observed' & !region == 'LDCs'), aes(year, gii.mean, color = scenario), size = 1) +
+  geom_line(data = gii.region %>%  filter(year >= 2015 & !region == 'LDCs'), aes(year, gii.mean, color = scenario), size = 1) +
+  #geom_ribbon(data = gii.region %>%  filter(year > 2015), aes(year, ymin= upp, ymax=low, color = scenario), alpha=0.2) +
+  facet_wrap(~region) + 
+  labs(y = 'Mean GII', x = 'Year', title = 'Gender Inequality Index (GII): regional projections', color = 'Scenario') +
+  ylim(0, 0.70) +
+  theme_minimal() +
+  theme(legend.direction = 'horizontal', 
+        legend.position = c(.725, .915),
+        legend.background = element_rect(fill = '#f5f5f5', size = 0),
+        legend.title = element_text(color = '#757575', size = 13),
+        legend.text = element_text(color = '#757575', size = 10),
+        #panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.background = element_rect(colour = "grey", size=0.5),
+        plot.title = element_text(color = '#757575'),
+        axis.title.x = element_text(color = '#757575'),
+        axis.title.y = element_text(color = '#757575'),
+        text = element_text(size=15), 
+        axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  scale_x_continuous(breaks= c(2000, 2020, 2040, 2060, 2080, 2100)) +
+  scale_color_manual(values=c("Observed" = "#05749E", "SSP1" = "#00BA38", "SSP2" = '#00BFC4', 
+                              'SSP3' = '#FF8C00', 'SSP4' = "#F564E3", 'SSP5' = '#AED602'))
+
+
+
+# LDCs
+
+ldcs <- gii.region %>% filter(region == 'LDCs' & !countrycode %in% c('AGO', 'MMR'))
+
+ggplot() +
+  geom_line(data = ldcs %>%  filter(year <= 2015 & scenario == 'Observed'), aes(year, gii, color = scenario), size = 1) +
+  geom_line(data = ldcs %>%  filter(year >= 2015), aes(year, gii, color = scenario)) +
+  facet_wrap(~countrycode) + 
+  labs(y = 'GII', x = 'Year', title = 'Gender Inequality Index (GII) projections for Least Developed Countries (LDCs)', color = 'Scenario') +
+  ylim(0, 1) +
+  theme(legend.position = 'bottom', text = element_text(size=12), axis.text.x = element_text(angle = 90)) +
+  scale_color_manual(values=c("Observed" = "#05749E", "SSP1" = "#00BA38", "SSP2" = '#00BFC4', 
+                              'SSP3' = '#FF8C00', 'SSP4' = "#F564E3", 'SSP5' = '#AED602'))
+
+
+# Stepwise regression to display in results (Supplementary Material)
+
+library(sandwich)
+
+m1 <- lm(gii.log ~ lngdppc + factor(countrycode), data = gii.reg)
+cov1 <- vcovHC(m1, type = 'HC')
+se1 <- sqrt(diag(cov1))
+
+m2 <- lm(gii.log ~ lngdppc + mys.gap + factor(countrycode), data = gii.reg)
+cov2 <- vcovHC(m2, type = 'HC')
+se2 <- sqrt(diag(cov2))
+
+m3 <- lm(gii.log ~ lngdppc + mys.gap + post.secondary + factor(countrycode), data = gii.reg)
+cov3 <- vcovHC(m3, type = 'HC')
+se3 <- sqrt(diag(cov3))
+
+m4 <- lm(gii.log ~ lngdppc + mys.gap + post.secondary, data = gii.reg)
+cov4 <- vcovHC(m4, type = 'HC')
+se4 <- sqrt(diag(cov4))
+
+
+stargazer(m1, m2, m3, m4, 
+          se = list(se1, se2, se3, se4),
+          title = 'Regression results: stepwise and fixed effects',
+          column.labels = c('FE', 'FE', 'FE', 'OLS'),
+          keep = c('lngdppc', 'mys.gap', 'post.secondary'),
+          dep.var.labels = 'Gender Inequality Index',
+          covariate.labels = c('GDP per capita', 'Gender gap in schooling', 'University education'),
+          omit.stat = "f",
+          align = TRUE)
+
+# Maps (Figure 1)
+
+library(viridis)
+library(ggthemes)
+library(maps)
+library(rworldmap)
+
+# Purple-green
+brewer.pal(n = 10, name = "PRGn")
+gii.palette2 <- c("#1B7837", "#5AAE61", "#E7D4E8", "#C2A5CF", "#9970AB", "#762A83", "#40004B")
+
+# Select data 
+
+ssp <- unique(gii.complete$scenario)
+
+for(s in ssp){
+dat <- gii.complete %>%
+  filter(year == 2017) %>% # Choose the period and the scenario 
+  rename(var = gii) # Choose the variable that is going to be shown on the map
+map <- joinCountryData2Map(dat, joinCode = "ISO3", nameJoinColumn = "countrycode")
+map_poly <-  fortify(map) %>% 
+  merge(map@data, by.x="id", by.y="ADMIN", all.x=T) %>%
+  arrange(id, order) %>% 
+  mutate(var %>% as.numeric())
+temp.map <- ggplot(map_poly, aes( x = long, y = lat, group = group)) +
+  coord_map(projection = 'mollweide', xlim = c(-180, 180), ylim = c(-60, 75))  + # Remove antarctica
+  geom_polygon(aes(fill = var)) +
+  scale_fill_gradientn(colors = gii.palette2, breaks = c(0.3, 0.6, 0.9), limits = c(0,1)) +
+  labs(fill = 'Value'
+       ,title = dat$scenario   # Change the title of the map
+       ,x = NULL
+       ,y = NULL) +
+  theme(text = element_text(family = 'Helvetica', color = 'gray40')
+        ,plot.title = element_text(size = 18)
+        ,axis.ticks = element_blank()
+        ,axis.text = element_blank()
+        ,axis.line = element_blank()
+        ,panel.grid = element_blank()
+        ,panel.background = element_rect(fill = 'white')
+        ,plot.background = element_rect(fill = 'white')
+        ,legend.position = c(.08,.26)
+        ,legend.background = element_blank()
+        ,legend.key = element_blank()
+  ) +
+  annotate(geom = 'text'
+           ,label = ''
+           ,x = 18, y = -55
+           ,size = 3
+           ,family = 'Helvetica'
+           ,color = 'gray50'
+           ,hjust = 'left'
+  )
+ggsave(temp.map, file=paste0("map_", s ,".pdf"), width = 16, height = 10, units = "cm")
+}
+
+
+# Matching with population (SDG-related analysis for Fig 4)
+
+gii.current <- gii.master %>% filter(year == 2017)
+
+gii.relative <- gii.complete %>%
+  filter(year %in% c(2017, 2020, 2030, 2050, 2100)) %>% 
+  select(countrycode, year, scenario, gii) %>% 
+  mutate(q = ifelse(gii <= 0.3, 'q1', 'q2'))
+
+# Interpolate population size to get values for year 2017 (latest observed)
+
+dpinterp <- function(df) {
+  with(df, approx(x=indx, y=value, xout=min(indx):max(indx))) %>% 
+    bind_cols()
+}
+
+pop.prep <- read.csv('Input/pop_size_fem_3.csv', skip = 8) %>% 
+  rename_all(tolower) %>% 
+  filter(scenario == 'SSP2' & year %in% c(2015:2020)) %>% 
+  mutate(scenario = 'Observed')
+
+pop.ipol <- pop.prep %>% 
+  group_by(area, scenario, age, sex) %>% 
+  rename(indx = year, value = population) %>% 
+  do(dpinterp(.)) %>% 
+  rename(year = x, population = y) %>% 
+  right_join(pop.prep %>% distinct(area), by = 'area')
+
+pop.size <-read.csv('Input/pop_size_fem_3.csv', skip = 8) %>% 
+  rename_all(tolower) %>%
+  filter(year == 2050) %>% 
+  bind_rows(pop.ipol) %>% 
+  mutate(countrycode = countrycode(area, 'country.name', 'iso3c')) %>% 
+  drop_na(countrycode) %>% 
+  filter(sex == 'Female' & age == '15+') %>% 
+  left_join(gii.relative, by = c('countrycode', 'year', 'scenario')) %>%
+  drop_na(gii) %>% 
+  group_by(year, scenario) %>% 
+  mutate(girls.total = sum(population)) %>% 
+  ungroup() %>% 
+  group_by(year, scenario, q) %>% 
+  mutate(girls.qs = sum(population)) %>% 
+  ungroup() %>% 
+  mutate(fract = girls.qs/girls.total * 100) %>% 
+  group_by(scenario, year) %>% 
+  arrange(desc(q)) %>%
+  mutate(lab.ypos = cumsum(fract) - 0.5*fract) %>% 
+  mutate(id = paste0(scenario, year, q)) %>% 
+  ungroup() %>% 
+  filter(!duplicated(id)) %>% 
+  mutate(q = q %>% as.factor())
+
+
+ggplot(pop.size %>% filter(year == 2017)) +
+  geom_bar(aes(x = 2, y = fract, fill = q), stat = "identity", color = "white") +
+  coord_polar(theta = "y", start = 0) +
+  #geom_text(x = 2, aes(y = lab.ypos, label = fract, color = "white"), size  = 4) +
+  scale_fill_brewer(type = 'seq', palette = 'Reds', direction = 1) +
+  labs(title = 'Girls affected 0--14 in 2017') +
+  theme_void() + 
+  xlim(0.5, 2.5) 
